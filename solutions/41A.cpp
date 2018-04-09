1a7b5a5f3ace4888bafc0f72383289606ef027a9
Yoshi <crispy245@gmail.com>
2018-04-09 21:13:01 +0900

    Add solution 41A

---
+++ solutions/41A.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define eprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define eprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  string t, s;
+  cin >> t >> s;
+  reverse(s.begin(), s.end());
+  string answer = "NO";
+  if(t == s) {
+    answer = "YES";
+  }
+  cout << answer << endl;
+  return 0;
+}
