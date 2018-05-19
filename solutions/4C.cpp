9136ee0139c77e3ca5288a4d53751b8124a3f01d
Yoshi <crispy245@gmail.com>
2018-05-19 17:19:08 +0900

    Add solution 4C

---
+++ solutions/4C.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  int n;
+  cin >> n;
+  map<string, int> m;
+  string tmp;
+  REP(i, n) {
+    cin >> tmp;
+    if(m[tmp] == 0) {
+      cout << "OK" << endl;
+    }
+    else {
+      cout << tmp + to_string(m[tmp]) << endl;
+    }
+    m[tmp]++;
+  }
+  return 0;
+}
