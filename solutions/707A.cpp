53f367c2b1c1dfd68b068165b9da49178983c93b
Yoshi <crispy245@gmail.com>
2018-06-10 22:53:09 +0900

    Add solution 707A

---
+++ solutions/707A.cpp
@@ -0,0 +1,54 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+  char c;
+  string answer = "#Black&White";
+  REP(i, m) {
+    REP(j, n) {
+      cin >> c;
+      if(c == 'C' || c == 'M' || c == 'Y') {
+        cout << "#Color" << endl;
+        return 0;
+      }
+    }
+  }
+  cout << answer << endl;
+  return 0;
+}
