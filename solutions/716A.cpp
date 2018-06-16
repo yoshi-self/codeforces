d1eb082e557a2b6363a19d73a13b5e8a8e652fda
Yoshi <crispy245@gmail.com>
2018-06-16 19:50:05 +0900

    Add solution 716A

---
+++ solutions/716A.cpp
@@ -0,0 +1,56 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  int n, c;
+  cin >> n >> c;
+  int answer = 0;
+  int prev = 0;
+  REP(i, n) {
+    int now;
+    cin >> now;
+    if(now - prev <= c) {
+      answer++;
+    }
+    else {
+      answer = 1;
+    }
+    prev = now;
+  }
+  cout << answer << endl;
+  return 0;
+}
