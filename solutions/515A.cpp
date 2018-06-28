75d2b4bfe84a52862d8f4d4b218609839f233e70
Yoshi <crispy245@gmail.com>
2018-06-28 14:39:41 +0900

    Add solution 515A

---
+++ solutions/515A.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  LL a, b, s;
+  cin >> a >> b >> s;
+  string answer = "Yes";
+  if(abs(a) + abs(b) > s || (s - (a + b)) % 2) {
+    answer = "No";
+  }
+  cout << answer << endl;
+
+  return 0;
+}
