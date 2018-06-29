e8837d0813dbc3010df2f4b5e265977761ce6a93
Yoshi <crispy245@gmail.com>
2018-06-29 21:47:23 +0900

    Add solution 237A

---
+++ solutions/237A.cpp
@@ -0,0 +1,55 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
+#include <complex>
+#include <functional>
+#include <utility>
+#include <iterator>
+
+#define REP(i, n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i, m, n) for(int i = (m); i < (int)(n); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define INF 2000000000
+
+#ifdef LOCAL
+  #define dprintf(...) fprintf(stdout, __VA_ARGS__)
+#else
+  #define dprintf(...) 0
+#endif
+
+using namespace std;
+
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef unsigned int uint;
+
+//const double PI = 3.1415926535897932384626433832795;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<vector<int>> v(24);
+  REP(i, v.size()) {
+    v[i].resize(60);
+  }
+  int answer = 1;
+  REP(i, n) {
+    int h, m;
+    cin >> h >> m;
+    v[h][m]++;
+    answer = max(answer, v[h][m]);
+  }
+  cout << answer << endl;
+
+  return 0;
+}
