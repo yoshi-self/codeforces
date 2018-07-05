fea6e9cd76c5a3732de7294c767c18e93270caa5
Yoshi <crispy245@gmail.com>
2018-07-05 23:07:14 +0900

    Add solution 630A

---
+++ solutions/630A.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cctype>
+#include <climits>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <string>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  cout << 25 << endl;
+  return 0;
+}
